df514919ebe2e995385753549065d0507c4d8486
Kaveh <kaveh.hadjari@b3it.se>
2018-09-03 10:41:50 +0200

    add functionality so redux exposes stores to redux timetravel chrome ext

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
 export default function configureStore(initialState={}) {
  return createStore(
   rootReducer,
-   applyMiddleware(thunk)
+   compose(
+      applyMiddleware(thunk),
+      /**
+       * Conditionally add the Redux DevTools extension enhancer
+       * if it is installed.
+       */
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
  );
 }
